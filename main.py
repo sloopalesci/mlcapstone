d538bd04cf5526a71c9ddc1e9f25c8c319acadfe
sloopalesci <sloopalesci@gmail.com>
2024-06-19 14:01:13 -0700

    final main add Jupyter

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from sklearn import linear_model, metrics, model_selection, svm
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
 import xgboost as xgb
 from sklearn.ensemble import AdaBoostClassifier, ExtraTreesClassifier
-# from sklearn.metrics import accuracy_score
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.naive_bayes import GaussianNB
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.linear_model import SGDClassifier
 # import numpy as np
 # from matplotlib import pyplot
 # from pandas.plotting import scatter_matrix
@@ -33,10 +28,6 @@ mygb_model = GradientBoostingClassifier()  # GradientBoostingClassifier model
 myxgb_model = xgb.XGBClassifier()  # xgboost model
 ab_model = AdaBoostClassifier(algorithm='SAMME')  # AdaBoostClassifier model
 et_model = ExtraTreesClassifier()  # ExtraTreesClassifier model
-# myknn_model = KNeighborsClassifier()  # KNeighborsClassifier model
-# dt_model = DecisionTreeClassifier()  # DecisionTreeClassifier model
-# sgd_model = SGDClassifier(loss='modified_huber')  # SGDClassifier model
-# mygnb_model = GaussianNB()  # GaussianNB model
 
 y = df.values[:, 34]
 X = df.values[:, 0:34]
@@ -50,10 +41,6 @@ mygb_model.fit(X_train, y_train)  # GradientBoostingClassifier model
 myxgb_model.fit(X_train, y_train)  # xgboost model
 ab_model.fit(X_train, y_train)  # AdaBoostClassifier model
 et_model.fit(X_train, y_train)  # ExtraTreesClassifier model
-# myknn_model.fit(X_train, y_train)  # KNeighborsClassifier model
-# dt_model.fit(X_train, y_train)  # DecisionTreeClassifier model
-# sgd_model.fit(X_train, y_train)  # SGDClassifier model
-# mygnb_model.fit(X_train, y_train)  # GaussianNB model
 
 # predict the outputs using the test data
 y_pred = mylog_model.predict(X_test)  # linear regression model
@@ -63,10 +50,6 @@ y_pred_gb = mygb_model.predict(X_test)  # GradientBoostingClassifier model
 y_pred_xgb = myxgb_model.predict(X_test)  # xgboost model
 y_pred_ab = ab_model.predict(X_test)  # AdaBoostClassifier model
 y_pred_et = et_model.predict(X_test)  # ExtraTreesClassifier model
-# y_pred_sgd = sgd_model.predict(X_test)  # SGDClassifier model
-# y_pred_knn = myknn_model.predict(X_test)  # KNeighborsClassifier model
-# y_pred_gnb = mygnb_model.predict(X_test)  # GaussianNB model
-# y_pred_dt = dt_model.predict(X_test)  # DecisionTreeClassifier model
 
 # print out  the prediction of the same output for all the models
 dataset = [
@@ -81,10 +64,6 @@ gbc_prediction = mygb_model.predict(dataset)
 xgboost_prediction = myxgb_model.predict(dataset)
 ab_prediction = ab_model.predict(dataset)
 et_prediction = et_model.predict(dataset)
-# sgd_prediction = sgd_model.predict(dataset)
-# knn_prediction = myknn_model.predict(dataset)
-# gnb_prediction = mygnb_model.predict(dataset)
-# dt_prediction = dt_model.predict(dataset)
 
 print("linear regression predicts:", linear_prediction, "and has an accuracy of:",
       mylog_model.score(X_test, y_test))
@@ -97,13 +76,6 @@ print("GradientBoostingClassifier predicts:", gbc_prediction, "and has an accura
 print("xgboost predicts:", xgboost_prediction, "and has an accuracy of:", myxgb_model.score(X_test, y_test))
 print("AdaBoost predicts:", ab_prediction, "and has an accuracy of:", ab_model.score(X_test, y_test))
 print("Extra Trees predicts:", et_prediction, "and has an accuracy of:", et_model.score(X_test, y_test))
-# print("Stochastic Gradient Descent predicts:", sgd_prediction, "and has an accuracy of:",
-#       sgd_model.score(X_test, y_test))
-# print("KNeighborsClassifier predicts:", knn_prediction, "and has an accuracy of:",
-#       myknn_model.score(X_test, y_test))
-# print("GaussianNB predicts:", gnb_prediction, "and has an accuracy of:",
-#       mygnb_model.score(X_test, y_test))
-# print("Decision Tree predicts:", dt_prediction, "and has an accuracy of:", dt_model.score(X_test, y_test))
 
 # print out the accuracy of the models
 voting_clf = VotingClassifier(estimators=[('lr', mylog_model), ('rf', myrf_model), ('svm', mysvm_model),
